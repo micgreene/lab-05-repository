6a48d4ab5f4cb764f758df85bbe72c9eb2863387
mikegreene <mgreene11111@yahoo.com>
2020-10-10 16:57:08 -0700

    initialized portfolio page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 //creating a server "live server-lite"
 
 //Bringing express library to my server
+require('dotenv').config();
 const express = require('express');
 
 //create an instance of express/start express
@@ -11,9 +12,25 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //create a pubic directory for serving files
-app.use(express.static('public'));
+app.use(express.static('/public'));
 
 //start our server
 app.listen(PORT, ()=>{
   console.log(`Welcome to the server on port: ${PORT}`);
-})
+});
+
+//Additional Functions Not Yet Covered
+// app.get('/hello', (request, response) => {
+//   response.status(200).send('Hello');
+// });
+
+// app.get('/data', (request, response) => {
+//   let airplanes = {
+//     departure: Date.now(),
+//     canFly: true,
+//     pilot: 'Well Trained',
+//   };
+//   response.status(200).json(airplanes);
+// });
+
+// app.use('*', (request, response) => response.send('Sorry, that route does not exist.'));
